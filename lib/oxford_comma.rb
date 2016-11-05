76c7bb5b50ce1aa16a2536265902a3b8420c2e48
Mohammed Chisti <mchisti@outlook.com>
2016-11-05 00:36:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+  to_sentence(array)
+end
 
+def to_sentence(array)
+  if array.length == 1
+    return array[0]
+  end
+
+  sentence = Array.new
+  0.upto(array.length - 2) { |i|
+    sentence.push(array[i])
+  }
+
+  if array.length > 2
+    sentence.push("and #{array[array.length-1]}")
+    string = sentence.join(", ")
+  else
+    string = sentence[0]
+    "#{string} and #{array[array.length-1]}"
+  end
 end
